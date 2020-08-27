88348fec415a4278a97aaf1114ca99bdcbe34e85
create-o-life <info@create-o-life.com>
2020-08-27 06:56:36 +0000

    cardlist img

--- components/List.vue
+++ components/List.vue
@@ -32,46 +32,7 @@
       >
         <template v-for="section in sections">
         <v-divider></v-divider>
-        <v-list-item
-          :to="`/${menu.id}/${section.id}`"
-          nuxt
-          :three-line="overview"
-        >
-          <v-list-item-content>
-            <v-list-item-subtitle
-              :style="`color: ${color.date};`"
-            >
-              {{ (section.date) ? $dayjs(section.date).format('YYYY年M月D日(ddd)') : $dayjs(section.updatedAt).format('YYYY年M月D日(ddd)') }}
-            </v-list-item-subtitle>
-            
-            <v-list-item-title
-              :style="`color: ${color.title};`"
-              class="font-weight-bold"
-            >
-              {{ section.title }}
-            </v-list-item-title>
-            
-            <v-list-item-subtitle
-              v-if="overview"
-              :style="`color: ${color.overview};`"
-            >
-              {{ section.overview }}
-            </v-list-item-subtitle>
-          </v-list-item-content>
-          <v-list-item-action
-            class="align-self-center"
-          >
-            <v-chip
-              v-if="section.category[0] != null"
-              :small="$vuetify.breakpoint.sm"
-              :x-small="$vuetify.breakpoint.xs"
-              :color="color.chip"
-              outlined
-            >
-              {{ section.category[0] }}
-            </v-chip>
-          </v-list-item-action>
-        </v-list-item>
+        <LineList :section="section" :menu="menu" />
         </template>
         <v-divider></v-divider>
       </v-list>
@@ -89,50 +50,7 @@
           md="4"
           class="align-self-stretch"
         >
-          <v-card
-            height="100%"
-            :color="color.bg"
-            :to="`/${menu.id}/${section.id}`"
-            elevation="1"
-            tile
-            nuxt
-          >
-            <v-card-title
-              :style="`color: ${color.title};`"
-              class="font-weight-bold"
-            >
-              {{ section.title }}
-            </v-card-title>
-            
-            <v-card-subtitle
-              :style="`color: ${color.date};`"
-            >
-              {{ (section.date) ? $dayjs(section.date).format('YYYY年M月D日(ddd)') : $dayjs(section.updatedAt).format('YYYY年M月D日(ddd)') }}
-            </v-card-subtitle>
-            
-            <template v-if="overview">
-            <v-divider></v-divider>
-            
-            <v-card-text
-              :style="`color: ${color.overview};`"
-            >
-              {{ section.overview }}
-            </v-card-text>
-            </template>
-            
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-chip
-                v-if="section.category[0] != null"
-                :small="$vuetify.breakpoint.sm"
-                :x-small="$vuetify.breakpoint.xs"
-                :color="color.chip"
-                outlined
-              >
-                {{ section.category[0] }}
-              </v-chip>
-            </v-card-actions>
-          </v-card>
+          <CardList :section="section" :menu="menu" />
         </v-col>
         </template>
       </v-row>
@@ -147,21 +65,13 @@ export default {
   data () {
     return {
       color: {
-        bg: '#fff',
-        title: '#333',
-        date: '#333',
-        overview: '#333',
-        chip: '#333'
+        bg: '#fff'
       }
     }
   },
   mounted () {
     this.color = {
-      bg: this.$config.COLOR_LIST_BG,
-      title: this.$config.COLOR_LIST_TITLE,
-      date: this.$config.COLOR_LIST_DATE,
-      overview: this.$config.COLOR_LIST_OVERVIEW,
-      chip: this.$config.COLOR_LIST_CHIP
+      bg: this.$config.COLOR_LIST_BG
     }
   },
   computed: {
@@ -172,22 +82,15 @@ export default {
         }
       }
     },
-    overview () {
-      if (this.menu.list != null) {
-        if (this.menu.list.display.includes('概要')) {
-          return true
-        }
-      }
-    },
     bgImg () {
       if (this.menu.list != null) {
         if (this.menu.list.bgImg) {
           const val = {
-              xs: 600,
-              sm: 960,
-              md: 1264,
-              lg: 1264,
-              xl: 1264
+            xs: 600,
+            sm: 960,
+            md: 1264,
+            lg: 1264,
+            xl: 1264
           }
           const size = this.bp(val)
           var color = this.color.bg
